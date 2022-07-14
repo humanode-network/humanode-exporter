3ab7c224632aa7fc71ec165a28a29c65ccdf93ae
MOZGIII <mike-n@narod.ru>
2022-07-14 18:52:04 +0400

    Only register metrics when they're available

--- src/registerMetrics.ts
+++ src/registerMetrics.ts
@@ -2,21 +2,25 @@ import { ApiPromise } from "@polkadot/api";
 import { Gauge } from "prom-client";
 
 export default (api: ApiPromise) => {
-  new Gauge({
-    name: "humanode_state_session_validators_count",
-    help: "count of session validators",
-    async collect() {
-      const validators = await api.query.session.validators();
-      this.set((validators.toJSON() as string[]).length);
-    },
-  });
+  if (typeof api?.query?.session?.validators === "function") {
+    new Gauge({
+      name: "humanode_state_session_validators_count",
+      help: "count of session validators",
+      async collect() {
+        const validators = await api.query.session.validators();
+        this.set((validators.toJSON() as string[]).length);
+      },
+    });
+  }
 
-  new Gauge({
-    name: "humanode_rpc_last_block_extrinsics_count",
-    help: "total number of extrinsics in the best block",
-    async collect() {
-      const block = await api.rpc.chain.getBlock();
-      this.set(block.block.extrinsics.length);
-    },
-  });
+  if (typeof api?.rpc?.chain?.getBlock === "function") {
+    new Gauge({
+      name: "humanode_rpc_last_block_extrinsics_count",
+      help: "total number of extrinsics in the best block",
+      async collect() {
+        const block = await api.rpc.chain.getBlock();
+        this.set(block.block.extrinsics.length);
+      },
+    });
+  }
 };
